2183e2678306b462906f1b508501bfe1dc9de220
frontend90 <leon44173@gmail.com>
2020-01-18 18:02:42 +0200

    сверстано меню

--- js/main.js
+++ js/main.js
@@ -3,15 +3,31 @@ $('.dropdown').click(function () {
   $('.menu').show(500);
   $('.menu').addClass('active');
 });
-
+/*************     закрытие по ссылке    ************/
 $('.menu a').click(function () {
-  $('.menu').hide(100);
+  $('.menu').hide(500);
+  $('.menu').removeClass('active');
+});
+/*************     закрытие по кнопке    ************/
+$('.close-menu').click(function () {
+  $('.menu').hide(500);
   $('.menu').removeClass('active');
 });
 
-$(document).mouseup(function () {
-      var popup = $('.menu');
-      popup.removeClass('active');
-        
-      
-      });
+/*********************     плавный скролл        ************************/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
+  });
+});
